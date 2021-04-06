70608640a73d4ebbc92f2097926a9efd5e2cd594
Lindaelva <78419144+Lindaelva@users.noreply.github.com>
2021-04-06 16:04:25 +0300

    Update 10.v_skolenu_darbi.py

--- 10.v_skolenu_darbi.py
+++ 10.v_skolenu_darbi.py
@@ -1,28 +1,28 @@
 #Tatjana Grencione
 
 #1)Andersone Linda Elva
+123
+#2)ApsÃ®tis Edvards
 
-#2)Apsîtis Edvards
+#3)ÃˆerÃ²eviÃ¨a LetÃ®cija LÃ®na
 
-#3)Èeròevièa Letîcija Lîna
+#4)Drozdova MiÃ°elle
 
-#4)Drozdova Miğelle
-
-#5)Karlinskis Renârs
+#5)Karlinskis RenÃ¢rs
 
 #6)Lukjanskis Miks Patriks
 
-#7)Lukğs Lauris
+#7)LukÃ°s Lauris
 
-#8)Mâlkalns Kristaps
+#8)MÃ¢lkalns Kristaps
 
-#9)Ruòìis Kristaps
+#9)RuÃ²Ã¬is Kristaps
 
-#10)Segliòğ Jçkabs
+#10)SegliÃ²Ã° JÃ§kabs
 
-#11)Serşante Agate
+#11)SerÃ¾ante Agate
 
-#12)Voiteğonoks Aleksandrs
+#12)VoiteÃ°onoks Aleksandrs
 
 #13)Zvaigzne Gustavs Henrijs
 
